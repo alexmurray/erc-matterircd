e97dda4a26fa9ae1415c4cd4d31a446362c98b1a
Alex Murray <murray.alex@gmail.com>
2020-08-19 14:46:20 +0930

    Add test for link handling

--- test/erc-matterircd-test.el
+++ test/erc-matterircd-test.el
@@ -29,6 +29,28 @@
                (buffer-substring (point-min) (point-max))
                (concat " " (propertize "bold" 'face 'bold) " "))))))
 
+(ert-deftest erc-matterircd-test-links ()
+  "Test that [link](url) gets handled appropriately."
+  (with-temp-buffer
+    (cl-letf (((symbol-function 'erc-network)
+               (lambda () 'matterircd)))
+      (insert " [text](url) ")
+      (erc-matterircd-format-links)
+      (should (ert-equal-including-properties
+               (buffer-substring (point-min) (point-max))
+               (concat " " (propertize "text" 'erc-matterircd-link-url "url") " ")))
+      (erc-matterircd-buttonize-links)
+      (should (ert-equal-including-properties
+               (buffer-substring (point-min) (point-max))
+               (concat " " (propertize "text"
+                                       'font-lock-face 'erc-button
+                                       'mouse-face 'highlight
+                                       'erc-callback 'browse-url-button-open-url
+                                       'keymap erc-button-keymap
+                                       'rear-nonsticky t
+                                       'erc-data '("url"))
+                       " "))))))
+
 (provide 'erc-matterircd-test)
 ;;; erc-matterircd-test.el ends here
 
