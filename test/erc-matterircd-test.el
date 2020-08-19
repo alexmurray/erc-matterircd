7789866da651ad3e96ed6d27984dbea33bbd124c
Alex Murray <murray.alex@gmail.com>
2020-08-19 14:30:23 +0930

    Add some simple unit tests

---
+++ test/erc-matterircd-test.el
@@ -0,0 +1,35 @@
+;;; erc-matterircd-test.el --- tests for matterircd integration for ERC         -*- lexical-binding: t; -*-
+
+;;; Commentary:
+
+(require 'ert)
+(require 'erc-matterircd)
+
+;;; Code:
+(ert-deftest erc-matterircd-test-italic ()
+  "Test that *italic* / _italic_ gets handled appropriately."
+  (with-temp-buffer
+    (cl-letf (((symbol-function 'erc-network)
+               (lambda () 'matterircd)))
+      (insert " *italic asterisk* _italic underscore_ ")
+      (erc-matterircd-format-italics)
+      (should (ert-equal-including-properties
+               (buffer-substring (point-min) (point-max))
+               (concat " " (propertize "italic asterisk" 'face erc-matterircd-italic-face)
+                       " " (propertize "italic underscore" 'face erc-matterircd-italic-face) " "))))))
+
+(ert-deftest erc-matterircd-test-bold ()
+  "Test that **bold**_ gets handled appropriately."
+  (with-temp-buffer
+    (cl-letf (((symbol-function 'erc-network)
+               (lambda () 'matterircd)))
+      (insert " **bold** ")
+      (erc-matterircd-format-bolds)
+      (should (ert-equal-including-properties
+               (buffer-substring (point-min) (point-max))
+               (concat " " (propertize "bold" 'face 'bold) " "))))))
+
+(provide 'erc-matterircd-test)
+;;; erc-matterircd-test.el ends here
+
+
